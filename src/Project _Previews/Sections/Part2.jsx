6b8539b53f0f43de73624df19ee1ed76328e25f6
callmeVineeth <vineethpv024@gmail.com>
2024-02-02 15:07:44 +0530

    files  added

--- src/Project _Previews/Sections/Part2.jsx
+++ src/Project _Previews/Sections/Part2.jsx
@@ -117,9 +117,9 @@ function Part2({ projectTitle,projectDesc,previewVideo1,previewVideo2,
         <h1 className=" font-bold text-xl mt-10">Getting Started</h1>
         <div className=" w-10/12 flex flex-col gap-4 rounded-lg overflow-x-scroll bg-gray-200 p-4">
           
+          <span> git clone </span>
           <span> npm install </span>
-          <span> npm install </span>
-          <span> npm install </span>
+          <span> npm start </span>
           
         </div>
 
